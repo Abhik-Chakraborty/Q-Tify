982e12e1e0986d9efa0133162cd790a7f0021d9d
Abhik-Chakraborty <abhik17.cfc@gmail.com>
2025-05-24 21:37:43 +0530

    Fix Navbar button color and Carousel functionality

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -28,6 +28,24 @@ const Carousel = ({data,renderCardComponent}) => {
                 nextEl: '.swiper-button-next',
                 prevEl: '.swiper-button-prev',
             }}
+            breakpoints={{
+                320: {
+                    slidesPerView: 1,
+                    spaceBetween: 20
+                },
+                480: {
+                    slidesPerView: 2,
+                    spaceBetween: 30
+                },
+                768: {
+                    slidesPerView: 3,
+                    spaceBetween: 40
+                },
+                1024: {
+                    slidesPerView: 4,
+                    spaceBetween: 40
+                }
+            }}
         >
             <Controls data={data} />
             <CarouselLeft />
